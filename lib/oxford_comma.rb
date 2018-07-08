e5f66d2defdb2c680759aaea31cd211a993d5f43
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 15:47:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
-end
+  
+  length = array.length 
+  
+  if length  <= 1  
+    return array.join
+  elsif length == 2 
+    return array.join(" and ")
+  else #string >= 3
+    last_item = array.pop
+    string_cs = array.join(", ")
+    new_string = string_cs + ", and #{last_item}"
+    return new_string
+  end #end if/else
+  
+end #end oxford_comma
