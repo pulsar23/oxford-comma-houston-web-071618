1d5a2c4239ae1b1906bf41b4a1424ede0d785d0c
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 15:47:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,15 +2,15 @@ def oxford_comma(array)
   
   length = array.length 
   
-    if length  <= 1  
-      return array.join
-    elsif length == 2 
-      return array.join(" and ")
-    else #string >= 3
-      last_item = array.pop
-      string_cs = array.join(", ")
-      new_string = string_cs + ", and #{last_item}"
-      return new_string
-  end #end case
+  if length  <= 1  
+    return array.join
+  elsif length == 2 
+    return array.join(" and ")
+  else #string >= 3
+    last_item = array.pop
+    string_cs = array.join(", ")
+    new_string = string_cs + ", and #{last_item}"
+    return new_string
+  end #end if/else
   
 end #end oxford_comma
