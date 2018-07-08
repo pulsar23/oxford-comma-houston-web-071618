7d32ec94ef2f859ed4029b0938cb981169ec2431
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 15:43:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,11 @@ def oxford_comma(array)
   
   length = array.length 
   
-  case length
-    when length <= 1  
+    if length  <= 1  
       return array.join
-    when length == 2 
+    elsif length == 2 
       return array.join(" and ")
-    when length >= 3
+    else #string >= 3
       last_item = array.pop
       string_cs = array.join(",")
       new_string = string_cs + " and #{last_item}"
