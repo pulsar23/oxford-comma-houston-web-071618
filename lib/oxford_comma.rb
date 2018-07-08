4a5cc335abcd470ae3db032e3131c1b7a77c738d
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 15:40:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,10 @@ def oxford_comma(array)
     when length == 2 
       return array.join(" and ")
     when length >= 3
-      return array.join(",")
+      last_item = array.pop
+      string_cs = array.join(",")
+      new_string = string_cs + " and #{last_item}"
+      return new_string
   end #end case
   
 end #end oxford_comma
